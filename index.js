f56245f0841f566526bf7a15efd625d8b668cc6f
coldblot <akshaygupta42@live.com>
2024-02-24 18:43:24 +0530

    pushed new

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express=require('express');
 
 const app=express();
 
+app.get('test',(req,res)=>{
+    res.send("Working!");
+})
+
 app.listen('2001',()=>{console.log('server started on port: 2001')})
